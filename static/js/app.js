da94e8a706ed31f3c7d5f9f142c670e8dbbcf9a2
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2021-01-31 22:03:55 -0800

    Realized that I was extracting the data from the wrong place.

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,28 @@
 // Fetch the JSON data and console log it
 d3.json("/samples.json").then(function(data) {
-    var metaData = [];
-    Object.entries(data.metadata).forEach(([key, value]) =>{
-            metaData.push(value);
+
+    // Display the data to understand
+    //console.log(data);
+
+    // Get the samples data
+    var sampleData = [];
+    Object.entries(data.samples).forEach(([key, value]) =>{
+        sampleData.push(value);
         })
-    //console.log(metaData);
-    var sample_values = metaData.map(x=>x.wfreq);
+    //console.log(sampleData);
+    
+    // Extract the sample_values from the samples data
+    var sampleValues = sampleData.map(x=>x.id);
+    //console.log(sampleValues);
+
+    // Sort data for from most to least
+    var sampleSorted = sampleValues.sort((first, second) => 
+    second - first);
+    //console.log(sampleSorted);
+
+    // Use slice to get the top 10 OTUs found in that individual
+    var sample_values = sampleSorted.slice(0,10);
     console.log(sample_values);
-    // console.log(data);
   })
 
 // Sort data for the top 10 OTUs found in that individual
