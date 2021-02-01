427a22c8a47d90c085d3234321f530113aad54d5
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2021-01-31 21:43:02 -0800

    Extracted the sample values from the data.

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,15 @@
 // Fetch the JSON data and console log it
 d3.json("/samples.json").then(function(data) {
-    console.log(data);
+    var metaData = [];
+    Object.entries(data.metadata).forEach(([key, value]) =>{
+            metaData.push(value);
+        })
+    //console.log(metaData);
+    var sample_values = metaData.map(x=>x.wfreq);
+    console.log(sample_values);
+    // console.log(data);
   })
 
+// Sort data for the top 10 OTUs found in that individual
+// var sortedData = data.sort((first, second) => 
+//     second.greekSearchResults - first.greekSearchResults);
