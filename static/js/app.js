627055d00fb18162658e2237996d11cf10a54699
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2021-02-05 21:17:06 -0800

    Added a div with plot as id in the html, reorganized the previous codes based on hints gotten from OH. Converted id to string, so the y-axis is showing properly.

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,3 @@
-// // This function is called when a dropdown menu item is selected
-// function updatePlotly() {
-//   // Use D3 to select the dropdown menu
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-//   var dataset = dropdownMenu.property("value");
-
-//   buildPlot(sample);
-
-// }
-
 // // Fetch the JSON data 
 // function buildPlot(sample){
   d3.json("/samples.json").then(function(data) {
@@ -26,33 +15,54 @@
       selectOption.attr("value", x).text(x);
     })
 
-    // // Get the samples data
-    // var id1 = data.samples[0];
-    // console.log(id1);
+    // Get the samples data
+    var id1 = data.samples[0];
+    console.log(id1);
 
-    // // Extract the sample_values from the samples data
-    // var sampleValue1 = id1.sample_values;
-    // //console.log(sampleValue1);
+    // Extract the sample_values from the samples data
+    var sampleValue1 = id1.sample_values;
+    //console.log(sampleValue1);
 
-    // // Sort data for from most to least
-    // var value1Sorted = sampleValue1.sort((first, second) => 
-    //   second - first);
-    // //console.log(value1Sorted); 
+    // Use slice to get the top 10 OTUs found in that individual
+    var value1Sorted = sampleValue1.slice(0, 10);
+    console.log(value1Sorted); 
 
-    // // Use slice to get the top 10 OTUs found in that individual
-    // var value1Top = value1Sorted.slice(0, 10);
-    // console.log(value1Top); 
+    var outIds1 = id1.otu_ids;
+    var slicedOutIds1 =  (outIds1.slice(0,10)).toString();
+    console.log(slicedOutIds1);
 
-    // var sortedData = data.samples.forEach(x =>
-    //   x.sort((first, second)=> second.sample_values - first.sample_values)
-    //   );
+    var outlabels1 = id1.otu_labels;
+    var slicedOutlabels1 =  outlabels1.slice(0,10);
+    console.log(slicedOutlabels1);
 
-    // var slicedData = sortedData.slice(0, 10);
+    var trace1 = {
+      x: value1Sorted,
+      y: slicedOutIds1,
+      text:slicedOutlabels1,
+      type: "bar",
+      orientation: "h"
+    };
 
-    // console.log(slicedData);
+    var layout ={
+      title: "Bar Chart",
+      xaxis: {
+          title: "Sample Values",
+          tickangle: -45,
+          automargin: true
+      },
+      yaxis: {
+          title: "OTU IDs"
+      },
+  };
+  
+  Plotly.newPlot("plot", [trace1], layout);
 
-  })
+})
 //}
 
-// Call updatePlotly() when a change takes place to the DOM
-// d3.selectAll("#selDataset").on("change", updatePlotly);
+// Using the onchange from the html
+// function optionChanged(id){
+//   console.log(id);
+//   filtered = data.filter(d=>d.id===id);
+//   newFunctionforChart(filtered);
+// }
